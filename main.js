b2cb7850f0034f7e07cfbd52200f2bc928a75a83
jarkrapob <jarkrapob.s@gmail.com>
2023-04-24 10:10:10 +0700

    up : init

---
+++ main.js
@@ -0,0 +1,51 @@
+// Import components
+function cardBox({ }) {
+    return (
+        <>
+
+        </>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <div className="hiBox">
+
+                    <div className="hiDiv">
+                        <p>ใหม่และโดดเด่น</p>
+                    </div>
+                    <div className="hiDiv">
+                        <p>ผู้ชาย</p>
+                    </div>
+                    <div className="hiDiv">
+                        <p>ผู้หญิง</p>
+                    </div>
+                    <div className="hiDiv">
+                        <p>เด็ก</p>
+                    </div>
+                    <div className="hiDiv">
+                        <p>ลดราคา</p>
+                    </div>
+                    <div className="hiDiv">
+                        <p>SNKRS</p>
+                    </div>
+                    <div className="magnifyingGlass">
+                        <i class="fa-solid fa-magnifying-glass fa-lg"></i>
+                    </div>
+
+
+                </div>
+                <div className="loBox">
+                    <h1>lobox</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+// Render
+const domRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(domRoot);
+root.render(<App />);
