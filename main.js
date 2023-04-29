1b93edf3fcc1b5684dc55fa988eefdfabad41e52
niceaman <l.salin88@gmail.com>
2023-04-29 16:43:41 +0700

    update magnifyind div

--- main.js
+++ main.js
@@ -1,48 +1,41 @@
 // Import components
-function cardBox({ }) {
-    return (
-        <>
-
-        </>
-    )
+function cardBox({}) {
+  return <></>;
 }
 
 function App() {
-    return (
-        <>
-            <div className="container">
-                <div className="hiBox">
-
-                    <div className="hiDiv">
-                        <p>ใหม่และโดดเด่น</p>
-                    </div>
-                    <div className="hiDiv">
-                        <p>ผู้ชาย</p>
-                    </div>
-                    <div className="hiDiv">
-                        <p>ผู้หญิง</p>
-                    </div>
-                    <div className="hiDiv">
-                        <p>เด็ก</p>
-                    </div>
-                    <div className="hiDiv">
-                        <p>ลดราคา</p>
-                    </div>
-                    <div className="hiDiv">
-                        <p>SNKRS</p>
-                    </div>
-                    <div className="magnifyingGlass">
-                        <i class="fa-solid fa-magnifying-glass fa-lg"></i>
-                    </div>
-
-
-                </div>
-                <div className="loBox">
-                    <h1>lobox</h1>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container">
+        <div className="hiBox">
+          <div className="hiDiv">
+            <p>ใหม่และโดดเด่น</p>
+          </div>
+          <div className="hiDiv">
+            <p>ผู้ชาย</p>
+          </div>
+          <div className="hiDiv">
+            <p>ผู้หญิง</p>
+          </div>
+          <div className="hiDiv">
+            <p>เด็ก</p>
+          </div>
+          <div className="hiDiv">
+            <p>ลดราคา</p>
+          </div>
+          <div className="hiDiv">
+            <p>SNKRS</p>
+          </div>
+          <div className="magnifyingDiv">
+            <i class="fa-solid fa-magnifying-glass fa-lg"></i>
+          </div>
+        </div>
+        <div className="loBox">
+          <h1>lobox</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 // Render
